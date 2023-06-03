d78c5e3db7a113424b6c1e2877d33284faa079ae
Ben-21 <bs@schaefer-inet.de>
2023-06-03 12:10:10 +0200

    added method numberWord

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,19 @@ public class Main {
 
     public static void main(String[] args){
 
+
+
+
+
     }
+
+    static public String numberWord(int number){
+        String[] numbers = {"Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
+
+        return numbers[number-1];
+
+
+    }
+
+
 }
